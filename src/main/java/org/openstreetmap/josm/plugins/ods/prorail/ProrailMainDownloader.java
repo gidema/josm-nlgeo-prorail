14afa9ae9ace5b79bc4bb74cd9afc7f28d059fb1
gidema <mail@gertjanidema.nl>
2016-01-03 12:12:04 +0100

    Brought code in sync with v0.6.6-snapshot

--- src/main/java/org/openstreetmap/josm/plugins/ods/prorail/ProrailMainDownloader.java
+++ src/main/java/org/openstreetmap/josm/plugins/ods/prorail/ProrailMainDownloader.java
@@ -8,26 +8,31 @@ import org.openstreetmap.josm.plugins.ods.arcgis.rest.AGRestFeatureSource;
 import org.openstreetmap.josm.plugins.ods.arcgis.rest.AGRestHost;
 import org.openstreetmap.josm.plugins.ods.entities.opendata.FeatureDownloader;
 import org.openstreetmap.josm.plugins.ods.entities.opendata.OpenDataLayerDownloader;
+import org.openstreetmap.josm.plugins.ods.entities.opendata.OpenDataLayerManager;
 import org.openstreetmap.josm.plugins.ods.entities.osm.OsmLayerDownloader;
 import org.openstreetmap.josm.plugins.ods.io.LayerDownloader;
 import org.openstreetmap.josm.plugins.ods.io.MainDownloader;
 import org.openstreetmap.josm.plugins.ods.prorail.gt.build.ProrailGtRailBuilder;
 
-public class ProrailDownloader extends MainDownloader {
-    final ProrailImportModule module;
-    final private OpenDataLayerDownloader opendataLayerDownloader;
-    final private LayerDownloader osmDownloader;
+public class ProrailMainDownloader extends MainDownloader {
+    private final ProrailImportModule module;
+    private OpenDataLayerDownloader opendataLayerDownloader;
+    private LayerDownloader osmLayerDownloader;
 
-    public ProrailDownloader(ProrailImportModule module) {
+    public ProrailMainDownloader(ProrailImportModule module) {
         super();
         this.module = module;
+    }
+    
+    @Override
+    public void initialize() throws Exception {
         opendataLayerDownloader = new ProrailOpenDataLayerDownloader(module);
-        osmDownloader = new OsmLayerDownloader(module);
+        osmLayerDownloader = new OsmLayerDownloader(module);
     }
 
     @Override
     protected LayerDownloader getOsmLayerDownloader() {
-        return osmDownloader;
+        return osmLayerDownloader;
     }
 
     @Override
@@ -37,25 +42,47 @@ public class ProrailDownloader extends MainDownloader {
 
     private class ProrailOpenDataLayerDownloader extends OpenDataLayerDownloader {
         private final AGRestHost host = new AGRestHost("BBK_spoorobjecten_001", "http://mapservices.prorail.nl/ArcGIS/rest/services/BBK_spoorobjecten/MapServer", 10000);
+        private final ProrailPrimitiveBuilder primitiveBuilder;
+        private final OpenDataLayerManager layerManager;
+
+        @SuppressWarnings("hiding")
+        private final OdsModule module;
 
         public ProrailOpenDataLayerDownloader(OdsModule module) {
             super(module);
+            this.module = module;
+            this.layerManager = module.getOpenDataLayerManager();
             this.addFeatureDownloader(createRailDownloader());
             this.addFeatureDownloader(createSwitchDownloader());
+            this.primitiveBuilder = new ProrailPrimitiveBuilder(module);
+
+        }
+        
+        @Override
+        public void process() {
+            try {
+                super.process();
+                primitiveBuilder.run(getResponse());
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw e;
+            }
         }
         
         private FeatureDownloader createRailDownloader() {
             ProrailGtRailBuilder entityBuilder = new ProrailGtRailBuilder(module.getCrsUtil());
             OdsFeatureSource featureSource = new AGRestFeatureSource(host, "Spoor/36");
             AGRestDataSource dataSource = new AGRestDataSource(featureSource);
-            return new AGRestDownloader(dataSource, module.getCrsUtil(), entityBuilder);
+            return new AGRestDownloader<>(dataSource, module.getCrsUtil(), entityBuilder,
+                    layerManager.getEntityStore(Rail.class));
         }
 
         private FeatureDownloader createSwitchDownloader() {
             ProrailGtRailBuilder entityBuilder = new ProrailGtRailBuilder(module.getCrsUtil());
             OdsFeatureSource featureSource = new AGRestFeatureSource(host, "Wisselbeen/37");
             AGRestDataSource dataSource = new AGRestDataSource(featureSource);
-            return new AGRestDownloader(dataSource, module.getCrsUtil(), entityBuilder);
+            return new AGRestDownloader<>(dataSource, module.getCrsUtil(), entityBuilder,
+                    layerManager.getEntityStore(Rail.class));
         }
     }
 }
