fce9564cacd70aa864f91cafd072a9d8c595c7f6
gidema <mail@gertjanidema.nl>
2017-03-29 14:46:10 +0200

    Fixed josm-ods issue 69: Introduce OdsProcessor interface

--- src/main/java/org/openstreetmap/josm/plugins/ods/prorail/ProrailOpenDataLayerDownloader.java
+++ src/main/java/org/openstreetmap/josm/plugins/ods/prorail/ProrailOpenDataLayerDownloader.java
@@ -1,5 +1,8 @@
 package org.openstreetmap.josm.plugins.ods.prorail;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.openstreetmap.josm.plugins.ods.OdsDataSource;
 import org.openstreetmap.josm.plugins.ods.OdsModule;
 import org.openstreetmap.josm.plugins.ods.domains.buildings.Entrance;
@@ -11,9 +14,11 @@ import org.openstreetmap.josm.plugins.ods.domains.railway.RailCrossing;
 import org.openstreetmap.josm.plugins.ods.domains.railway.RoadCrossing;
 import org.openstreetmap.josm.plugins.ods.domains.railway.Switch;
 import org.openstreetmap.josm.plugins.ods.entities.Entity;
+import org.openstreetmap.josm.plugins.ods.entities.PrimitiveBuilder;
 import org.openstreetmap.josm.plugins.ods.entities.opendata.FeatureDownloader;
 import org.openstreetmap.josm.plugins.ods.entities.opendata.OpenDataLayerDownloader;
 import org.openstreetmap.josm.plugins.ods.exceptions.OdsException;
+import org.openstreetmap.josm.plugins.ods.io.OdsProcessor;
 
 public class ProrailOpenDataLayerDownloader extends OpenDataLayerDownloader {
 
@@ -21,11 +26,11 @@ public class ProrailOpenDataLayerDownloader extends OpenDataLayerDownloader {
 
         public ProrailOpenDataLayerDownloader(OdsModule module) {
             super(module);
+            this.primitiveBuilder = new ProrailPrimitiveBuilder(getModule());
         }
 
         @Override
         public void initialize() throws OdsException {
-//            super.initialize();
             this.addFeatureDownloader(createDownloader("Geleidingssysteem_001:Spooras/3", Rail.class));
             this.addFeatureDownloader(createDownloader("Geleidingssysteem_001:Wissel/4", Switch.class));
             this.addFeatureDownloader(createDownloader("Geleidingssysteem_001:Kruising/5", RailCrossing.class));
@@ -34,24 +39,21 @@ public class ProrailOpenDataLayerDownloader extends OpenDataLayerDownloader {
             this.addFeatureDownloader(createDownloader("BBK_spoorobjecten_003:Overweg/20", RoadCrossing.class));
             this.addFeatureDownloader(createDownloader("BBK_spoorobjecten_003:Scherm/10", Barrier.class));
             this.addFeatureDownloader(createDownloader("BBK_spoorobjecten_003:Deur/9", Entrance.class));
-            this.primitiveBuilder = new ProrailPrimitiveBuilder(getModule());
         }
 
-        @Override
-        public void process() throws OdsException {
-//            try {
-                super.process();
-                primitiveBuilder.run(getResponse());
-                updateLayer();
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//                throw e;
-//            }
-        }
-    
         private <T extends Entity> FeatureDownloader createDownloader(String sourceName, Class<T> clazz) throws OdsException {
             OdsDataSource dataSource = getModule().getConfiguration().getDataSource(sourceName);
             dataSource.initialize();
             return dataSource.getOdsFeatureSource().getHost().createDownloader(getModule(), dataSource, clazz);
+        }
+
+        @Override
+        protected PrimitiveBuilder getPrimitiveBuilder() {
+            return primitiveBuilder;
+        }
+
+        @Override
+        protected List<Class<? extends OdsProcessor>> getProcessors() {
+            return Collections.emptyList();
         }
     }
